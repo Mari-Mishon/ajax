7dfcca16f3b6adbf332f35f1c3b2aaeac7b1a3e9
Mari-Mishon <mashenka-777@bk.ru>
2018-03-10 22:26:21 +0700

    M

---
+++ add.php
@@ -0,0 +1,61 @@
+<?php
+$n = count($_POST);
+$key = array_keys($_POST);
+$json= json_encode($_POST, 256);
+for($i=0; $i < $n; $i++){
+$fp = fopen('results.json', 'w');
+fwrite($fp, pretty_json($json));
+fclose($fp);	
+}
+
+function pretty_json($json) {
+ 
+    $result      = '';
+    $pos         = 0;
+    $strLen      = strlen($json);
+    $indentStr   = '  ';
+    $newLine     = "\n";
+    $prevChar    = '';
+    $outOfQuotes = true;
+ 
+    for ($i=0; $i<=$strLen; $i++) {
+ 
+        // Grab the next character in the string.
+        $char = substr($json, $i, 1);
+ 
+        // Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+ 
+        // If this character is the end of an element, 
+        // output a new line and indent the next line.
+        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos --;
+            for ($j=0; $j<$pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+ 
+        // Add the character to the result string.
+        $result .= $char;
+ 
+        // If the last character was the beginning of an element, 
+        // output a new line and indent the next line.
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos ++;
+            }
+ 
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+ 
+        $prevChar = $char;
+    }
+ 
+    return $result;
+}
+?>
